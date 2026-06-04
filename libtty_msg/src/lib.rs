20af4e03e605bff89e57e063961b1481c79a347f
Cheese-Space <batumete1011@gmail.com>
2026-06-04 21:57:46 +0200

    define message struct

--- libtty_msg/src/lib.rs
+++ libtty_msg/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::Serialize;
+use serde::Deserialize;
+use std::fmt;
+#[derive(Serialize, Deserialize)]
+pub struct Message {
+    pub(crate) user: String,
+    pub(crate) msg: String
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl fmt::Display for Message {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}", self.user, self.msg)
     }
 }
