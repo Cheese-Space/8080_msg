e52a7afd4d6cdd667840b8a6bf9daecbeb298129
Cheese-Space <batumete1011@gmail.com>
2026-06-29 18:24:45 +0200

    add simple argument parsing

--- tty_8080/src/main.rs
+++ tty_8080/src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// the ip adress of the server
+    #[arg(short, long, value_name = "ADRESS")]
+    adress: String,
+    /// the port to connect to
+    #[arg(short, long)]
+    port: u16
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    
 }
