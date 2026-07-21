fe9470a0e317d1d139f6e3d4f11e034ce0907f52
Cheese-Space <batumete1011@gmail.com>
2026-07-21 18:15:41 +0200

    handle sending file transfers

--- tty_8080/src/main.rs
+++ tty_8080/src/main.rs
@@ -15,10 +15,9 @@ struct Args {
 }
 trait ErrorString: Into<StyledString> + Send + 'static {}
 impl<T: Into<StyledString> + Send + 'static> ErrorString for T {}
-type CursiveCallBack = Box<dyn FnOnce(&mut Cursive) + Send + 'static>;
 /// create a new error which won't exit
-fn non_fatal_error<S: ErrorString>(text: S) -> CursiveCallBack {
-    Box::new(|siv| {
+fn non_fatal_error<S: ErrorString>(text: S) -> impl FnOnce(&mut Cursive) {
+    |siv| {
         siv.add_layer(
             Dialog::new()
                 .content(TextView::new(text))
@@ -28,11 +27,11 @@ fn non_fatal_error<S: ErrorString>(text: S) -> CursiveCallBack {
                 .title("error")
                 .h_align(HAlign::Center),
         );
-    })
+    }
 }
 /// create a new error which will exit
-fn fatal_error<S: ErrorString>(text: S) -> CursiveCallBack {
-    Box::new(|siv| {
+fn fatal_error<S: ErrorString>(text: S) -> impl FnOnce(&mut Cursive) {
+    |siv| {
         siv.add_layer(
             Dialog::new()
                 .content(TextView::new(text))
@@ -42,15 +41,15 @@ fn fatal_error<S: ErrorString>(text: S) -> CursiveCallBack {
                 .title("error")
                 .h_align(HAlign::Center),
         );
-    })
+    }
 }
 /// decides if the message provided should or shouldn't be displayed
 fn should_display_text(split_text: &[&str]) -> bool {
     let len = split_text.len();
     // we know that the message is never empty, so index zero should always be valid
     match split_text[0] {
-        "/exit" | "/getport" => false,
-        "/kick" | "file" if len == 2 => false,
+        "/exit" | "/getport" | "/file" => false,
+        "/kick" if len == 2 => false,
         "/set_privilege" if len == 3 => false,
         _ => true,
     }
@@ -85,7 +84,7 @@ async fn actual_main() -> Result<(), Box<dyn std::error::Error>> {
             // only errors on early eof
             if reader.read_exact(&mut len_buff).await.is_err() {
                 cb_cink
-                    .send(fatal_error("error: lost connection with server"))
+                    .send(Box::new(fatal_error("error: lost connection with server")))
                     .expect("cursive cb channel should only close when program terminates");
                 return;
             }
@@ -93,7 +92,7 @@ async fn actual_main() -> Result<(), Box<dyn std::error::Error>> {
             let mut raw_contents: Vec<u8> = vec![0; size_to_read];
             if reader.read_exact(&mut raw_contents).await.is_err() {
                 cb_cink
-                    .send(fatal_error("error: lost connection with server"))
+                    .send(Box::new(fatal_error("error: lost connection with server")))
                     .expect("cursive cb channel should only close when program terminates");
                 return;
             }
@@ -101,10 +100,10 @@ async fn actual_main() -> Result<(), Box<dyn std::error::Error>> {
                 Ok(c) => c,
                 Err(e) => {
                     cb_cink
-                        .send(non_fatal_error(format!(
+                        .send(Box::new(non_fatal_error(format!(
                             "error: server send malformed data: {e}\nrecieved: {:?}",
                             raw_contents
-                        )))
+                        ))))
                         .expect("cursive cb channel should only close when program terminates");
                     continue;
                 }
@@ -126,30 +125,111 @@ async fn actual_main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(async move {
         let cb_cink = cb_clone;
         let username = u_clone;
+        let writer = Arc::new(Mutex::new(writer));
         while let Some(msg) = rx.recv().await {
             let split_message: Vec<&str> = msg.split_whitespace().collect();
+            let len = split_message.len();
             let msg = match split_message[0] {
                 "/exit" => Packet::Exit,
                 "/getport" => Packet::GetPort,
-                "/kick" if split_message.len() == 2 => Packet::Kick(split_message[1].to_string()),
-                "/set_privilege" if split_message.len() == 3 => {
+                "/kick" if len == 2 => Packet::Kick(split_message[1].to_string()),
+                "/set_privilege" if len == 3 => {
                     let privilege = match UserPrivilege::try_from(split_message[2]) {
                         Ok(p) => p,
                         Err(e) => {
                             let e = e.to_string();
-                            cb_cink.send(non_fatal_error(format!("error: {e}"))).expect(
-                                "cursive cb channel should only close when program terminates",
-                            );
+                            cb_cink
+                                .send(Box::new(non_fatal_error(format!("error: {e}"))))
+                                .expect(
+                                    "cursive cb channel should only close when program terminates",
+                                );
                             continue;
                         }
                     };
                     Packet::SetPrivilege(Some(split_message[1].to_string()), privilege)
                 }
+                "/file" => {
+                    // file transfers are handled in a different thread so we can still send messages if reading takes a long itme
+                    let writer = Arc::clone(&writer);
+                    let cb_cink = cb_cink.clone();
+                    let username = username.clone();
+                    tokio::spawn(async move {
+                        let (sender, reciever) = oneshot::channel::<(String, String)>();
+                        let sender = StdMutex::new(Some(sender));
+                        let cb_clone = cb_cink.clone();
+                        cb_cink.send(Box::new(move |siv| {
+                            siv.add_layer(Dialog::new().content(
+                                LinearLayout::vertical()
+                                    .child(TextView::new("enter path:"))
+                                    .child(EditView::new().with_name("path"))
+                                    .child(TextView::new("enter message:"))
+                                    .child(EditView::new().with_name("message"))
+                            ).button("send", move |siv| {
+                                let mut path = None;
+                                siv.call_on_name("path", |view: &mut EditView| {
+                                    let input = view.get_content();
+                                    if input.trim().is_empty() {
+                                        let error = non_fatal_error("error: no path provided");
+                                        cb_clone.send(Box::new(error)).expect("cursive cb channel should only close when program terminates");
+                                        return;
+                                    }
+                                    path = Some(input.to_string());
+                                });
+                                let mut message = None;
+                                siv.call_on_name("message", |view: &mut EditView| {
+                                    let input = view.get_content();
+                                    if input.trim().is_empty() {
+                                        let error = non_fatal_error("error: no message provided");
+                                        cb_clone.send(Box::new(error)).expect("cursive cb channel should only close when program terminates");
+                                        return;
+                                    }
+                                    message = Some(input.to_string());
+                                });
+                                let path = match path {
+                                    Some(p) => p,
+                                    None => return,
+                                };
+                                let message = match message {
+                                    Some(m) => m,
+                                    None => return,
+                                };
+                                let Some(sender) = sender.lock().unwrap().take() else {
+                                    unreachable!("should only be called once");
+                                };
+                                sender.send((path, message)).expect("reciever shouldn't have closed yet");
+                                siv.pop_layer();
+                            })
+                            .button("cancel", |siv| {siv.pop_layer();})
+                            .h_align(HAlign::Center));
+                        })).expect("cursive cb channel should only close when program terminates");
+                        let (path, msg) = match reciever.await {
+                            Ok(pm) => pm,
+                            Err(_) => return, // this means cancel was pressed
+                        };
+                        let file = match UserFile::new_async(&path).await {
+                            Ok(f) => f,
+                            Err(e) => {
+                                cb_cink.send(Box::new(non_fatal_error(format!("error: failed to read file: {e}")))).expect("cursive cb channel should only close when program terminates");
+                                return;
+                            }
+                        };
+                        let msg = Message::new(&username, &msg);
+                        let packet = Packet::File(FileTransfer::new(msg, file));
+                        if packet.send_async(&mut *writer.lock().await).await.is_err() {
+                            cb_cink
+                                .send(Box::new(fatal_error("error: lost connection with server")))
+                                .expect(
+                                    "cursive cb channel should only close when program terminates",
+                                );
+                        }
+                    });
+                    continue;
+                }
                 _ => Packet::Msg(Message::new(&username, &msg)),
             };
-            if msg.send_async(&mut writer).await.is_err() {
+            if msg.send_async(&mut *writer.lock().await).await.is_err() {
                 cb_cink
-                    .send(fatal_error("error: lost connection with server"))
+                    .send(Box::new(fatal_error("error: lost connection with server")))
                     .expect("cursive cb channel should only close when program terminates");
                 return;
             }
